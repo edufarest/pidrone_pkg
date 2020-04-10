34b4282e6fb555062c196a93133b15268c9484d0
Arun Jeevanantham <itsmearun98@gmail.com>
2020-04-10 16:19:55 -0400

    get closest point on line

--- scripts/line_detector.py
+++ scripts/line_detector.py
@@ -20,20 +20,46 @@ def findCenter(lines):
 
 def isOnLine(cur_pos, lines):
     for line in lines:
-        x1, y1, x2, y2 = line[0]
-        p1 = np.asarray((x1, y1))
-        p2 = np.asarray((x2, y2))
-        p3 = np.asarray(cur_pos)
-        d = np.linalg.norm(np.cross(p2 - p1, p1 - p3)) / np.linalg.norm(p2 - p1)
+        d = getDistanceToLine(cur_pos, line)
         if d < DISTANCE_TO_LINE_CONST:
             return True
 
     return False
 
+def getDistanceToLine(cur_pos, line):
+    x1, y1, x2, y2 = line[0]
+    p1 = np.asarray((x1, y1))
+    p2 = np.asarray((x2, y2))
+    p3 = np.asarray(cur_pos)
+    d = np.linalg.norm(np.cross(p2 - p1, p1 - p3)) / np.linalg.norm(p2 - p1)
+    return d
+
+def getClosestLine(cur_pos, lines):
+    cur_min = 1000000000
+    min_line = None
+    for line in lines:
+        d = getDistanceToLine(cur_pos, line)
+        if d < cur_min:
+            cur_min = d
+            min_line = line
+
+    return min_line
 
 
-img = cv2.imread("images/line2.jpeg")
+def getClosestPointOnLine(cur_pos, lines):
+    min_line = getClosestLine(cur_pos, lines)
+    x3, y3 = cur_pos
+    x1, y1, x2, y2 = min_line[0]
+    dx = x2 - x1
+    dy = y2 - y1
+    d2 = dx * dx + dy * dy
+    nx = ((x3 - x1) * dx + (y3 - y1) * dy) / d2
+    return (dx * nx + x1, dy * nx + y1)
+
+
+img = cv2.imread("images/line.jpeg")
 height, width, channels = img.shape
+center = (width/2, height/2)
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 lower_blue = np.array([30, 50, 60])
 upper_blue = np.array([255, 255, 255])
@@ -56,9 +82,11 @@ for line in lines:
 print isOnLine((width/2, height/2), lines)
 
 center_x, center_y = findCenter(lines)
-
+closest_x, closest_y = getClosestPointOnLine(center, lines)
 
 cv2.circle(img, (center_x, center_y), 6, [0, 0, 255], 6)
+cv2.circle(img, (closest_x, closest_y), 6, [0, 255, 255], 6)
+
 cv2.imshow("linesEdges", img)
 
 # cv2.imshow('frame', img)
