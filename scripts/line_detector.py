7f017100da1a3c291d446e0c2db997787db960f5
Arun Jeevanantham <itsmearun98@gmail.com>
2020-04-10 16:01:40 -0400

    added is on line function

--- scripts/line_detector.py
+++ scripts/line_detector.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
-import numpy.linalg
+
+DISTANCE_TO_LINE_CONST = 3
 
 def findCenter(lines):
     center_x = 0
@@ -18,22 +19,21 @@ def findCenter(lines):
 
 
 def isOnLine(cur_pos, lines):
-    x, y = cur_pos
-
     for line in lines:
         x1, y1, x2, y2 = line[0]
         p1 = np.asarray((x1, y1))
         p2 = np.asarray((x2, y2))
         p3 = np.asarray(cur_pos)
-        d = norm(np.cross(p2 - p1, p1 - p3)) / norm(p2 - p1)
+        d = np.linalg.norm(np.cross(p2 - p1, p1 - p3)) / np.linalg.norm(p2 - p1)
+        if d < DISTANCE_TO_LINE_CONST:
+            return True
+
+    return False
 
 
-def distanceBetweenLines(pos_1, pos_2):
-    x, y = pos_1
-    x1, y1 = pos_2
-    return (x1 - x) ^ 2 + (y1 - y)^2
 
-img = cv2.imread("images/line.jpeg")
+img = cv2.imread("images/line2.jpeg")
+height, width, channels = img.shape
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 lower_blue = np.array([30, 50, 60])
 upper_blue = np.array([255, 255, 255])
@@ -50,9 +50,10 @@ edges = cv2.Canny(result, 140, 150)
 lines = cv2.HoughLinesP(edges, 1, np.pi/180, 50, maxLineGap=10)
 for line in lines:
     x1, y1, x2, y2 = line[0]
-    print line
+    print
     cv2.line(img, (x1, y1), (x2, y2), (0, 0, 128), 10)
 
+print isOnLine((width/2, height/2), lines)
 
 center_x, center_y = findCenter(lines)
 
